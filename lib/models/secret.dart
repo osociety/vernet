9738baf24c01dc5e2cf02adead7ac80ff4b93948
Paras <fs0c19ty@protonmail.com>
2021-06-25 08:48:42 +0530

    bug fix for fdroid

--- lib/models/secret.dart
+++ lib/models/secret.dart
@@ -1,7 +1,34 @@
+import 'package:encrypt/encrypt.dart';
+import 'package:crypto/crypto.dart';
+import 'dart:convert'; // for the utf8.encode method
+
 class Secret {
   final String ipifyKey;
   Secret({this.ipifyKey = ""});
+
+  static String _createKey() {
+    String key = ")cz-Q8VSPZdfeft{q9_:p>@+N@M,y-L~";
+    List<int> codeUnits = key.codeUnits;
+    List<int> mCodes = [];
+    for (int i = 0; i < codeUnits.length; i++) {
+      mCodes.add((codeUnits[i] * 2) % 26);
+    }
+    return String.fromCharCodes(mCodes);
+  }
+
+  static String _createDigest() {
+    var bytes = utf8.encode(_createKey());
+    var digest = sha1.convert(bytes);
+    return digest.toString().substring(0, 32);
+  }
+
   factory Secret.fromJson(Map<String, dynamic> jsonMap) {
-    return new Secret(ipifyKey: jsonMap["ipify_key"]);
+    String ipifyKey = jsonMap["ipify_key"];
+    String digest = _createDigest();
+    final key = Key.fromUtf8(digest);
+    final iv = IV.fromLength(16);
+    final encrypter = Encrypter(AES(key));
+    final decrypted = encrypter.decrypt64(ipifyKey, iv: iv);
+    return new Secret(ipifyKey: decrypted);
   }
 }
