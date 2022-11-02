b0c1be16eb07254d373a4fb4ddf1c8cee6420f1d
guyluz11 <guyluz11@gmail.com>
2022-11-02 13:15:23 +0200

    Updated all packages and clean pubspec.yaml from default comments

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,7 +6,7 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <network_info_plus_windows/network_info_plus_windows_plugin.h>
+#include <network_info_plus/network_info_plus_windows_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
