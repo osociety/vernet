3f290318957cc8c675aa582542e9490733df76cc
git-elliot <fs0c19ty@protonmail.com>
2024-03-31 19:35:51 +0530

    add yaml changes

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,13 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <nsd_windows/nsd_windows_plugin_c_api.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  NsdWindowsPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("NsdWindowsPluginCApi"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
