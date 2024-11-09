4d9197210fa811b4736b41acd6032a5d09f79428
git-elliot <fs0c19ty@protonmail.com>
2024-11-09 13:49:00 +0530

    fix

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:network_tools_flutter/network_tools_flutter.dart';
@@ -13,7 +13,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:vernet/injection.dart';
 import 'package:vernet/main.dart';
 import 'package:vernet/ui/adaptive/adaptive_list.dart';
-import 'package:vernet/values/keys.dart';
+// import 'package:vernet/values/keys.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
