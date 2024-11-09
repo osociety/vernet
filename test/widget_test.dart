c3e42a078c0ac563e8bbfabd471797c4f73c13cd
git-elliot <fs0c19ty@protonmail.com>
2024-11-09 12:19:20 +0530

    test fixes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,17 +6,9 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
-import 'package:network_tools_flutter/network_tools_flutter.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:vernet/injection.dart';
 import 'package:vernet/main.dart';
 
 Future<void> main() async {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  configureDependencies(Env.test);
-  final appDocDirectory = await getApplicationDocumentsDirectory();
-  await configureNetworkToolsFlutter(appDocDirectory.path);
   group('end-to-end test', () {
     testWidgets('tap on the scan for devices button, verify device found',
         (tester) async {
