3575dedf0e6b814f6f2d73482c241966b605cc40
git-elliot <fs0c19ty@protonmail.com>
2024-11-10 15:44:49 +0530

    duration added

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -48,32 +48,10 @@ void main() {
 
       // Emulate a tap on the button.
       await tester.tap(devicesButton);
-      await tester.pumpAndSettle();
+      await tester.pump(const Duration(seconds: 2));
       expect(find.byType(AdaptiveListTile), findsAny);
-      await tester.pumpAndSettle();
+      await tester.pumpAndSettle(const Duration(seconds: 10));
       expect(find.byType(AdaptiveListTile), findsAtLeast(2));
-
-      // final routerIconButton = find.byKey(Keys.routerOrGatewayTileIconButton);
-
-      // await tester.tap(routerIconButton);
-      // await tester.pumpAndSettle();
-      // expect(find.byType(AppBar), findsOne);
-
-      // final radioButton = find.byKey(Keys.rangePortScanRadioButton);
-      // await tester.tap(radioButton);
-      // await tester.pumpAndSettle();
-
-      // final portChip = find.byKey(Keys.knownPortChip);
-
-      // await tester.tap(portChip);
-      // await tester.pumpAndSettle();
-
-      // final portScanButton = find.byKey(Keys.portScanButton);
-      // await tester.tap(portScanButton);
-      // await tester.pumpAndSettle(const Duration(seconds: 20));
-      // await tester.pump();
-      // // No port is opened in github actions
-      // expect(find.byType(AdaptiveListTile), findsNothing);
     });
 
     testWidgets('port scan returns open port for google.com', (tester) async {
