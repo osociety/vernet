63bcaf9d5927fd3f73d0aa856030cce755701375
git-elliot <fs0c19ty@protonmail.com>
2025-01-18 20:06:47 +0530

    Added for in=app internet

--- integration_test/settings/settings_test.dart
+++ integration_test/settings/settings_test.dart
@@ -1,7 +1,9 @@
 import 'dark_theme_test.dart' as dark_theme_test;
+import 'in_app_internet_test.dart' as in_app_internet_test;
 import 'subnet_tests.dart' as subnet_test;
 
 void main() {
   dark_theme_test.main();
   subnet_test.main();
+  in_app_internet_test.main();
 }
